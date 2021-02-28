8f6c428827609e0d06798979e61bd8b570c864d7
Arthur <whackon@163.com>
2021-02-28 18:12:41 +0800

    创建车辆模块项目、合作单位模块项目

--- socip-car-consumer/src/main/java/cn/ekgc/socip/SocipCarConsumerStarter.java
+++ socip-car-consumer/src/main/java/cn/ekgc/socip/SocipCarConsumerStarter.java
@@ -1,21 +1,21 @@
 package cn.ekgc.socip;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
- * <b>智慧公务车信息平台-系统功能 Provider 启动类</b>
+ * <b>智慧公务车信息平台-车辆功能 Consumer 启动类</b>
  * @author Arthur
  * @version 1.0.0
  * @since 1.0.0
  */
-@MapperScan("cn.ekgc.socip.*.dao")
+@EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
-public class SocipSystemProviderStarter {
+public class SocipCarConsumerStarter {
 	public static void main(String[] args) {
-		SpringApplication.run(SocipSystemProviderStarter.class, args);
+		SpringApplication.run(SocipCarConsumerStarter.class, args);
 	}
 }
--- socip-unit-provider/src/main/java/cn/ekgc/socip/SocipUnitProviderStarter.java
+++ socip-unit-provider/src/main/java/cn/ekgc/socip/SocipUnitProviderStarter.java
@@ -6,16 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
- * <b>智慧公务车信息平台-系统功能 Provider 启动类</b>
+ * <b>智慧公务车信息平台-合作单位功能 Provider 启动类</b>
  * @author Arthur
  * @version 1.0.0
  * @since 1.0.0
  */
-@MapperScan("cn.ekgc.socip.*.dao")
+@MapperScan("cn.ekgc.*.socip.dao")
 @EnableEurekaClient
 @SpringBootApplication
-public class SocipSystemProviderStarter {
+public class SocipUnitProviderStarter {
 	public static void main(String[] args) {
-		SpringApplication.run(SocipSystemProviderStarter.class, args);
+		SpringApplication.run(SocipUnitProviderStarter.class, args);
 	}
 }
